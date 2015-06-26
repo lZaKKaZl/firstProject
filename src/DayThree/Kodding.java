732002e9442e7de62268bd9552643dc30b8ff57f
Pavel <Pavel@test.com>
2015-06-26 20:55:11 +0300

    asdasd

---
+++ src/DayThree/Kodding.java
@@ -0,0 +1,13 @@
+package DayThree;
+
+/**
+ * Created by ITHILLEL5 on 26.06.2015.
+ */
+public class Kodding {
+    public static void main(String[] args) {
+        String message = "Hello";
+
+        char key = 'a';
+        int[]
+    }
+}
